a3eb8776dd08db25605f93e577ad039a987e5873
Harri Hämäläinen <hhamalai@iki.fi>
2015-04-07 17:06:01 +0300

    ...

---
+++ exercises/plot_smear.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as pl
+
+
+smear = pd.read_csv(
+    "../datasets/weather-kumpula.csv",
+    header=0,
+    index_col=0,
+    parse_dates=True)
+
+df = pd.DataFrame(smear)
+pl.figure()
+ax1 = df.t.plot(style='r-', legend=False, title="Weather 2014 (Kumpula, Helsinki)")
+ax2 = df.rmm.plot(secondary_y=True, style='b.')
+ax1.set_ylabel("average temperature per day (celcius)")
+ax2.right_ax.set_ylabel("cumulated rainfall per day (mm)")
+pl.savefig("foo.png")
--- exercises/smear.py
+++ exercises/smear.py
@@ -2,20 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pl
 
-# -> ts valmiiksi luettavaan muotoon
-# -> valmiiksi päiväkohtaisesti aggretoitu
-
 smear = pd.read_csv("../datasets/smear.csv", header=0, skiprows=range(1, 5))
 
 smear.rename(columns={'Unnamed: 0': 'ts'}, inplace=True)
 smear['ts'] = pd.to_datetime(smear['ts'])
 smear = smear.set_index('ts')
 
-df = pd.DataFrame(smear, index=smear.index, columns=('t', 'rmm'))
-df = df.resample("D", how={'t': np.mean, 'rmm': np.sum})
+df = pd.DataFrame(smear, index=smear.index)
+df = df.resample("D", how={'t': np.mean, 'rmm': np.sum, 'dp': np.mean, 'rh': np.mean, 'p': np.mean, 'ws': np.mean, 'wdir': 'last'})
 
-ax1 = df.t.plot(style='r-', legend=False, title="Weather 2014 (Kumpula, Helsinki)")
-ax2 = df.rmm.plot(secondary_y=True, style='b.')
-ax1.set_ylabel("average temperature per day (celcius)")
-ax2.right_ax.set_ylabel("cumulated rainfall per day (mm)")
-pl.show()
+df.to_csv("../datasets/weather-kumpula.csv")
